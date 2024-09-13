c25f68faaf3244cabd45fad2668f321511759628
snowfudge <150321282+snowfudge@users.noreply.github.com>
2024-09-13 12:25:33 +0700

    rewrite: ggn trade value calculator 2.0
    - add icon
    - use jquery
    - code cleanup
    - minor css adjustments

--- ggn-trade-value-calculator.user.js
+++ ggn-trade-value-calculator.user.js
@@ -1,123 +1,91 @@
 // ==UserScript==
 // @name         GGn Trade Value Calculator
 // @namespace    https://gazellegames.net/
-// @version      1.3.2
-// @description  This will show items value in the trade window and the estimated min/max amount of trade value required to complete a trade
+// @version      2.0
+// @description  Show Items' Values and Required Amount to Complete a Trade
 // @author       snowfudge
+// @icon         https://icons.duckduckgo.com/ip3/gazellegames.net.ico
 // @homepage     https://github.com/snowfudge/ggn-userscripts
 // @downloadURL  https://github.com/snowfudge/ggn-userscripts/raw/main/ggn-trade-value-calculator.user.js
 // @match        https://gazellegames.net/user.php?action=trade*
 // ==/UserScript==
 
-const stringToInt = (num) => {
-  return parseInt(num.replace(/,/g, ""));
+const stringToInt = (string) => {
+  return parseInt(string.replace(/,/g, ""));
 };
 
 const parseNumber = (num) => {
   return Math.round(num).toLocaleString("en-US");
 };
 
-const whoNeedsFiller = (myValue, theirValue) => {
-  if (myValue < theirValue) {
-    requiredInfo.style.background = "rgba(255, 0, 0, 0.7)";
-    return "You";
+const myValueEl = $("#my_trade_value_gold").get(0);
+const theirValue = stringToInt($("#other_trade_value_gold").text());
+const requiredInfo = `<div id="required-info" style="display: none; padding: 10px 0; margin-bottom: 20px; border-radius: 8px"><span></span> need around <strong style="font-size: 16px;"></strong> worth of trade value to complete this trade.</div>`;
+$(".message_box").prepend(requiredInfo);
+
+const requiredAmount = $("#required-info").find("strong");
+const requiredSide = $("#required-info").find("span");
+const myInitialValue = stringToInt(myValueEl.innerText);
+
+const parseInfo = (me, other) => {
+  if (me === 0 && other === 0) {
+    $("#required-info").hide();
   } else {
-    requiredInfo.style.background = "rgba(0, 0, 0, 0.7)";
-    return "They";
+    $("#required-info").show();
+  }
+
+  if (me > other) {
+    $("#required-info").css("background", "#1C1C1C");
+    $(requiredAmount).text(
+      `${parseNumber(me / 1.5)} to ${parseNumber(me * 1.5)}`
+    );
+    $(requiredSide).text("They");
+  } else if (me < other) {
+    $("#required-info").css("background", "#B10000");
+    $(requiredAmount).text(
+      `${parseNumber(other / 1.5)} to ${parseNumber(other * 1.5)}`
+    );
+    $(requiredSide).text("You");
   }
 };
 
-const createInfoDiv = () => {
-  const div = document.createElement("div");
-  div.id = "required_info";
-  div.style =
-    "clear: both; background: rgba(0, 0, 0, 0.5); color: #fff; padding: 10px; margin-bottom: 15px; font-size: 16px;";
-  div.innerHTML = `<span></span> need around <strong></strong> worth trade value to complete this trade.`;
+(() => {
+  ("use strict");
 
-  document
-    .querySelector(".message_box")
-    .insertAdjacentElement("afterbegin", div);
-};
+  $("li.item").each(function () {
+    $(this).find("img:not(.equipment_level)").css({
+      position: "absolute",
+      left: 0,
+      right: 0,
+      "max-height": "64px",
+      "max-width": "64px",
+      margin: "0 auto",
+    });
 
-const showItemValue = () => {
-  const items = document.querySelectorAll("li.item");
+    $(this).find("h3").css({
+      margin: 0,
+      bottom: "30px",
+    });
 
-  items.forEach((el) => {
-    const value = el.getAttribute("data-cost");
-    el.querySelector("h3").style.bottom = "15px";
-
-    el.insertAdjacentHTML(
-      "beforeend",
-      `<span style="background: #000; color: #DDA82A; position: absolute; bottom: 2px; padding: 3px; left: 0; right: 0;">${parseNumber(
-        value
+    $(this).append(
+      `<span style="position: absolute; bottom: 5px; left: 0; width: 100%; color: #DDA82A;">${parseNumber(
+        $(this).data("cost")
       )}</span>`
     );
   });
-};
 
-showItemValue();
-createInfoDiv();
-
-const requiredInfo = document.getElementById("required_info");
-const requiredAmount = requiredInfo.querySelector("strong");
-const requiredParty = requiredInfo.querySelector("span");
-
-(function () {
-  ("use strict");
-
-  const acceptButton = document.getElementById("accept_trade");
-
-  const myValueEl = document.getElementById("my_trade_value_gold");
-  const myValue = stringToInt(myValueEl.textContent);
-
-  const theirValueEl = document.getElementById("other_trade_value_gold");
-  const theirValue = stringToInt(theirValueEl.textContent);
-
-  if (myValue === 0 && theirValue === 0) {
-    requiredInfo.style.display = "none";
-  }
-
-  let myInitialMinValue = theirValue / 1.5,
-    myInitialMaxValue = theirValue * 1.5,
-    theirInitialMinValue = myValue / 1.5,
-    theirInitialMaxValue = myValue * 1.5,
-    initialValueInfo;
-
-  if (myValue > theirValue) {
-    // They need
-    initialValueInfo = `~${parseNumber(theirInitialMinValue)} to ~${parseNumber(
-      theirInitialMaxValue
-    )}`;
-  } else if (myValue < theirValue) {
-    // I need
-    initialValueInfo = `~${parseNumber(myInitialMinValue)} to ~${parseNumber(
-      myInitialMaxValue
-    )}`;
-  }
-
-  requiredParty.textContent = whoNeedsFiller(myValue, theirValue);
-  requiredAmount.textContent = initialValueInfo;
+  parseInfo(myInitialValue, theirValue);
 
   const obsConfig = { childList: true };
-  const observer = new MutationObserver(function (mutationsList, observer) {
+  const observer = new MutationObserver(function (_, observer) {
     observer.disconnect();
 
-    const acceptButtonDisabled = acceptButton.hasAttribute("disabled");
-    const myNewValue = stringToInt(myValueEl.textContent);
+    const myValue = stringToInt(myValueEl.innerText);
 
-    requiredParty.textContent = whoNeedsFiller(myNewValue, theirValue);
-
-    requiredInfo.style.display = acceptButtonDisabled ? "block" : "none";
-
-    if (myNewValue > theirValue) {
-      requiredAmount.textContent = `~${parseNumber(
-        myNewValue / 1.5
-      )} to ~${parseNumber(myNewValue * 1.5)}`;
-    } else if (myNewValue < theirValue) {
-      requiredAmount.textContent = initialValueInfo;
-    }
+    parseInfo(myValue, theirValue);
 
     observer.observe(myValueEl, obsConfig);
   });
+
   observer.observe(myValueEl, obsConfig);
 })();
