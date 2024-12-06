812886ec395e8d4d0e0ae9a8d370808e1cde7eed
snowfudge <150321282+snowfudge@users.noreply.github.com>
2024-12-06 10:15:45 +0700

    feat: add allowed forums fo reply autofocus in Forum Games only
    - defaults to Forum Games only
    - add ggn icon for consistency

--- ggn-forums-reply-autofocus.user.js
+++ ggn-forums-reply-autofocus.user.js
@@ -1,17 +1,23 @@
 // ==UserScript==
 // @name         GGn Forums: Reply Autofocus
 // @namespace    https://gazellegames.net/
-// @version      1.0.1
-// @description  This will scroll down to the end of the page and focus your cursor on the quick reply area
+// @version      1.1
+// @description  Automatically focus on quick reply in forum threads
 // @author       snowfudge
+// @icon         https://icons.duckduckgo.com/ip3/gazellegames.net.ico
 // @homepage     https://github.com/snowfudge/ggn-userscripts
 // @downloadURL  https://github.com/snowfudge/ggn-userscripts/raw/main/ggn-forums-reply-autofocus.user.js
 // @match        https://gazellegames.net/forums.php?*action=viewthread&threadid=*
-
 // ==/UserScript==
+
+const allowedForums = ["Forum Games"];
 
 (function () {
   "use strict";
 
-  document.getElementById("quickpost").focus();
+  const currentForum = $("#content > div.thin > h2:first-child a:eq(1)").text();
+
+  if (allowedForums.includes(currentForum)) {
+    $("#quickpost").focus();
+  }
 })();
