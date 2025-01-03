0967f48683957fe83c51338952f7854e701b269a
snowfudge <150321282+snowfudge@users.noreply.github.com>
2025-01-03 10:55:06 +0700

    fix incorrect update link, ugh

--- ggn-expand-trade-history.user.js
+++ ggn-expand-trade-history.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         GGn Expand Trade History
 // @namespace    https://gazellegames.net/
-// @version      1.1
+// @version      1.1.1
 // @description  Automatically expand all hidden items in trade history
 // @author       snowfudge
 // @icon         https://icons.duckduckgo.com/ip3/gazellegames.net.ico
 // @homepage     https://github.com/snowfudge/ggn-userscripts
-// @downloadURL  https://github.com/snowfudge/ggn-userscripts/raw/main/ggn-show-all-trade-history.user.js
+// @downloadURL  https://github.com/snowfudge/ggn-userscripts/raw/main/ggn-expand-trade-history.user.js
 // @match        https://gazellegames.net/user.php?*action=all_trades*
 // ==/UserScript==
 
